b38c581d38ff5449180b3d73b5093e8340eaee23
mjimenez2014 <jimenezmaury@gmail.com>
2017-04-04 22:25:11 -0300

    UpdateRiffo

--- app/assets/javascripts/compmanual/new.js
+++ app/assets/javascripts/compmanual/new.js
@@ -8,13 +8,15 @@ $('#boton').click(function(ev){
 	var url1 = 'http://localhost:3000/contribuyentes/busca_por_rut?utf8=%E2%9C%93&rut=';
 	var url2 = $("#recipient-name").val();
 	var url3 = '&commit=buscar'
-	$.getJSON(url1+url2+url3, function(response) { 
+
+	$.getJSON(url1+url2+url3, function(response) {
 		$("#RutCliente").text("Rut:  " + response.rut);
 		$("#RzsCliente").text("Razón Social: " + response.nombre);		
 		//$("#idRutEmisor").css("display", "block");
 		$("#compmanual_rutemisor").val(response.rut);
 		//$("#idRznSocEmisor").css("display", "block");
 		$("#compmanual_rznsocemisor").val(response.nombre);
+		console.log(response);   		
 	});
 	$("#recipient-name").val(""); 
 	$("#compmanual_tipodoc").focus();
