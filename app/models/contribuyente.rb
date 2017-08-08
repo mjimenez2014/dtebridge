74a7a24192b0667ccfe24e196b2eeb6e46b8e1cb
mjimenez2014 <jimenezmaury@gmail.com>
2017-08-08 16:51:55 -0300

    AssetsPrecomplie

--- app/models/contribuyente.rb
+++ app/models/contribuyente.rb
@@ -1,7 +1,7 @@
 class Contribuyente < ActiveRecord::Base
-  validates :rut, :presence => {:message => "Este campo no puede estar vacío!"}, length: {minimum: 10, maximum: 10, :message => "RUT Ejemplo: 19987987-K"},:uniqueness=> {:message => " el Contribuyente ya existe!!!"}
-  validates :nombre, :presence => {:message => "Este campo no puede estar vacío!"}
-  validates :email, :presence => {:message => "Este campo no puede estar vacío!"}
+  validates :rut, :presence => {:message => "Este campo no puede estar en blanco!"}, length: {minimum: 10, maximum: 10, :message => "RUT Ejemplo: 19987987-K"},:uniqueness=> {:message => " el Contribuyente ya existe!!!"}
+  validates :nombre, :presence => {:message => "Este campo no puede estar en blanco!"}
+  validates :email, :presence => {:message => "Este campo no puede estar en blanco!"}
 
   require 'csv'
   def self.import(file)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Dtebridge::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   #config.assets.precompile =  ['*.js','*.js.coffee.erb', '*.css', '*.css.scss'] 
-
+  config.assets.precompile += %w( *.js *.css )
   # Generate digests for assets URLs.
   config.assets.digest = true
 
