7bf8c2dacab024194333075f2b5ec71d03e9e0c4
mjimenez2014 <jimenezmaury@gmail.com>
2017-08-13 23:31:55 -0300

    Update_El_Sultan_02

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Dtebridge::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   #config.assets.precompile =  ['*.js','*.js.coffee.erb', '*.css', '*.css.scss'] 
-  config.assets.precompile += %w( *.js *.css )
+  config.assets.precompile += %w( *.js, *.css, *js.coffee.erb )
   # Generate digests for assets URLs.
   config.assets.digest = true
 
