847a9abf0c7995f3a567e3a9850a0d15aab95e88
mjimenez2014 <jimenezmaury@gmail.com>
2017-08-21 22:22:11 -0300

    AddResumenLibros

--- app/controllers/detlibro_controller.rb
+++ app/controllers/detlibro_controller.rb
@@ -1,6 +1,9 @@
 class DetlibroController < ApplicationController
+
   def index
-    libro = Libro.find(params[:id])
-    @detalles =  libro.detlibro.order(tipodte: :asc , folio: :asc )
+    @libro = Libro.find(params[:id])
+    @detalles =  @libro.detlibro.order(tipodte: :asc , folio: :asc )
+
+    @detlibro = @libro.detlibro.select('tipodte,sum("mntiva") as mntiva,tipodte,sum("mntneto") as mntneto,sum("mntsincred") as mntsincred,sum("mnttotal") as mnttotal,sum("mntexe") as mntexe, count(*) as count').group('"tipodte"')
   end
 end
